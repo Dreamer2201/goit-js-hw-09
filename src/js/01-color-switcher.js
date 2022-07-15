903bae717735c0ef5f6c0db23053b2bf199ae3ea
Oksana Polyakova <ddreamer22011986@gmail.com>
2022-07-15 17:11:07 +0300

    Task#1

---
+++ src/js/01-color-switcher.js
@@ -0,0 +1,22 @@
+const startBtnEl = document.querySelector('button[data-start]');
+console.log(startBtnEl);
+const stopBtnEl = document.querySelector('button[data-stop]');
+console.log(stopBtnEl);
+const bodyEl = document.querySelector('body');
+
+startBtnEl.addEventListener('click', onStartBtnClick);
+stopBtnEl.addEventListener('click', onStopBtnClick);
+
+let intervalId = null;
+
+function onStartBtnClick() {
+    intervalId = setInterval(getRandomHexColor, 1000, 1000);
+    startBtnEl.setAttribute('disabled', 'true');
+};
+function onStopBtnClick() {
+    clearInterval(intervalId);
+    startBtnEl.removeAttribute('disabled', 'true');
+};
+function getRandomHexColor() {
+    return bodyEl.style.backgroundColor = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+};
