9a7e0fd90d99e1178010ed28bacfbf5d7880e4e3
Oksana Polyakova <ddreamer22011986@gmail.com>
2022-07-28 15:25:24 +0300

    Task#3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,46 +1,64 @@
 
+import Notiflix from 'notiflix';
+
   const  formEl = document.querySelector('.form');
-  const delayInputEl = document.querySelector('input[name="delay"]');
-  const stepInputEl = document.querySelector('input[name="step"]');
+  const firstDelayInputEl = document.querySelector('input[name="delay"]');
+  const stepDelayInputEl = document.querySelector('input[name="step"]');
   const amountInputEl = document.querySelector('input[name="amount"]');
   const btnSubmitEl = document.querySelector('button');
-
-  console.log(formEl);
-
-  formEl.addEventListener('submit', onSubmitForm);
-
-  function onSubmitForm(event) {
-    event.preventDefault();
-     
-    const amount = event.currentTarget.elements[2].value;
-    const delay = event.currentTarget.elements[0].value;
-    const stepDelay = event.currentTarget.elements[1].value;
-    console.log(amount);
-
-    for(let position = 0; position < amount; position++) {
-      
-      const promise = new Promise((resolve, reject) => {
-        const shouldResolve = Math.random() > 0.3;
-
-          setTimeout(() => {
-            if (shouldResolve) {
-              resolve('✅ Fulfilled promise ${position} in ${delay}ms');
-            } else {
-              reject('❌ Rejected promise ${position} in ${delay}ms');
-            }
-            return promise;
-          }, stepDelay);
-          
-      });
-      promise.then(result => {
-        console.log(result);
-      }).catch(error => {
-        console.log(error);
-      })
-
-    }
-  }
-
-
   
+
+  function onSubmitForm (event) {
+    event.preventDefault();
+    const stepDelay = stepDelayInputEl.value;
+    const firstDelay = firstDelayInputEl.value;
+    const amount = event.currentTarget.elements[2].value;
+     
+    for (let position = 1; position <= amount; position ++) {
+      console.log(position);
+      setTimeout(() => createPromise(position, stepDelay)
+      .then(({ position, stepDelay }) => {
+        
+        Notiflix.Notify.info(`✅ Fulfilled promise ${position} in ${stepDelay}ms`,
+        {
+          timeout: 6000, 
+        },);
+        // console.log(`✅ Fulfilled promise ${position} in ${stepDelay}ms`);
+        })
+      .catch(({ position, stepDelay }) => {
+        Notiflix.Notify.info(`❌ Rejected promise ${position} in ${stepDelay}ms`,
+        {
+          timeout: 6000,
+        },);
+        // console.log(`❌ Rejected promise ${position} in ${stepDelay}ms`);
+        }, firstDelay))
+        
+ } 
+ 
+  }
+ 
+  function createPromise(position, stepDelay) {
+
+   const shouldResolve = Math.random() > 0.3;
+
+   return new Promise ((resolve, reject) => {
+    setTimeout(() => {
       
+      if(shouldResolve) {
+        resolve({position, stepDelay})
+      } else {
+        reject({position, stepDelay});
+      }
+    }, position * stepDelay);
+   
+   }); 
+};
+
+function thanksGiven() {
+  Notiflix.Notify.info('It was such a hard to do, but interesting! Thank you for your work!',
+        {
+          timeout: 6000,
+        },);
+}
+
+formEl.addEventListener('submit', onSubmitForm);
