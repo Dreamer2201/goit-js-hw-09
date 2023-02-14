8d5ccac6b870c7f02f483afc4e166e6d6b979dd4
Oksana Polyakova <ddreamer22011986@gmail.com>
2023-02-14 12:26:01 +0200

    active dots in slider

--- src/js/carusel.js
+++ src/js/carusel.js
@@ -1,7 +1,7 @@
 const nextBtn = document.getElementById('nextImg');
 const prevBtn = document.getElementById('prevImg');
 const imgs = document.querySelectorAll('.elem');
-
+const dots = document.querySelectorAll('.dotImg');
 let index = 0;
 
 const toggleActiveSlide = ind => {
@@ -10,27 +10,35 @@ const toggleActiveSlide = ind => {
     }
    imgs[ind].classList.add('active');
 }
-
+const toggleActiveDot = ind => {
+    for (item of dots) {
+        item.classList.remove('active-dot');
+    } dots[ind].classList.add('active-dot');
+}
 const nextImg = () => {
     if(index === (imgs.length - 1)) {
         index = 0;
         toggleActiveSlide(index);
+        toggleActiveDot(index);
     } else {
         index ++;
         toggleActiveSlide(index);
+        toggleActiveDot(index);
     }
 }
 const prevImg = () => {
     if(index == 0) {
         index = (imgs.length - 1);
         toggleActiveSlide(index);
+        toggleActiveDot(index);
     } else {
         index --;
         toggleActiveSlide(index);
+        toggleActiveDot(index);
     }
 }
 
 nextBtn.addEventListener('click', nextImg);
 prevBtn.addEventListener('click', prevImg);
 
-setInterval(nextImg, 3000);
+setInterval(nextImg, 5000);
