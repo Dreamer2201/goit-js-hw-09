3806a2e1027abdfc62cf210b703de29fdde84c7c
Dreamer2201 <97681346+Dreamer2201@users.noreply.github.com>
2023-02-16 19:51:47 +0000

    Deploying to gh-pages from @ Dreamer2201/goit-js-hw-09@a3a8583930c9ca4fdc8c762e3feb49cea5929261 🚀

---
+++ partials/generete-text.dead99f9.js
@@ -0,0 +1,2 @@
+var lineBox=document.querySelector(".lineBeforeBox");lineBox.addEventListener("click",(function(e){lineBox.classList.toggle("long")}));
+//# sourceMappingURL=generete-text.dead99f9.js.map
